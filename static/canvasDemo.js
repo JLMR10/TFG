b79a5f0f065878375136aee3f4a6671eda05773f
Unknown <artronluc@alum.us.es>
2020-01-21 22:21:17 +0100

    Demo V1.01 Fixed js script

--- static/canvasDemo.js
+++ static/canvasDemo.js
@@ -1,10 +1,17 @@
 
-var canvas = document.querySelector('canvas');
+$(function init() {
+    createCanvas()
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+})
 
-var c = canvas.getContext('2d');
+function createCanvas() {
+    var canvas = document.querySelector('canvas');
 
-c.fillRect(100,100,100,100);
-c.fillStyle = 'red';
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    var c = canvas.getContext('2d');
+
+    c.fillRect(100,100,100,100);
+    c.fillStyle = 'red';
+}
